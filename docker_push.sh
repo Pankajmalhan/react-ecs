0f51d5850d8ef49360afa6ca244b7380b6361c29
Pankajmalhan <pankajmalhan30@gmail.com>
2019-04-09 08:27:07 +0530

    3rd commit

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
+docker build -t react-app .
+docker tag react-app:latest 806107407018.dkr.ecr.ap-south-1.amazonaws.com/react-app:latest
 docker push 806107407018.dkr.ecr.ap-south-1.amazonaws.com/react-app:latest
